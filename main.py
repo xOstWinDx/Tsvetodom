335e700f18fe852344555bd6a87668416e94a5cc
Starobogatov Alexey <Starobogatov.a@yandex.ru>
2024-05-08 14:39:39 +0300

    jinja templates

--- main.py
+++ main.py
@@ -8,6 +8,21 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-app.add_exception_handler(404, lambda x, y: RedirectResponse('/index.html'))
+app.add_exception_handler(404, lambda r, y: templates.TemplateResponse(request=r, name='not_found.html'))
 
-app.mount("/", StaticFiles(directory="static"), name="static")
+app.mount("/pages", StaticFiles(directory="static"), name="pages")
+
+
+@app.get('/')
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name='index.html')
+
+
+@app.get('/info')
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name='info.html')
+
+
+@app.get('/products')
+async def products(request: Request):
+    return templates.TemplateResponse(request=request, name='products.html')
