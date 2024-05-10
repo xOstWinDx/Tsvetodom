a277e2fe2ecd226487f78e2e33f47218f5b4f850
Starobogatov Alexey <Starobogatov.a@yandex.ru>
2024-05-10 20:06:29 +0300

    new footer and main page

--- main.py
+++ main.py
@@ -1,49 +1,19 @@
-from typing import Generic, T
 
 from fastapi import FastAPI, Depends
 from fastapi_pagination import add_pagination, Params
-
 from fastapi_pagination.ext.sqlalchemy import paginate
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Session
-from fastapi_pagination.links import Page as BasePage
+
+
 from starlette.requests import Request
 
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
 from database import get_db
-from products.dao import ProductDAO
 from products.models import Product
-from products.schemas import ProductOut
-
-
-class Page(BasePage[T], Generic[T]):
-    @property
-    def info(self) -> str:
-        ru_title = {
-            'first': "Начало",
-            'last': "Конец",
-            'self': "Наверх",
-            'next': "Дальше",
-            'prev': "Назад",
-        }
-
-        def _get_wrap_link(title: str, link: str) -> str:
-            if link is None:
-                return ''
-            return f'<a href="{link}"><button>{ru_title[title]}</button><a/>'
-
-        gg = self.links.model_dump(include={'first', 'prev', 'next', 'last'})
-        opt = []
-        opt.append(_get_wrap_link('first', gg['first']))
-        opt.append(_get_wrap_link('prev', gg['prev']))
-        opt.append(_get_wrap_link('next', gg['next']))
-        opt.append(_get_wrap_link('last', gg['last']))
-        options = "\n".join(opt)
-        return f"<div>{options}</div>"
-
+from products.schemas import ProductOut, Page
 
 app = FastAPI()
 
--- products/schemas.py
+++ products/schemas.py
@@ -1,7 +1,36 @@
+from typing import Generic, T
+
 from pydantic import BaseModel, Field
+from fastapi_pagination.links import Page as BasePage
 
 
 class ProductOut(BaseModel):  # define your model
     id: int = Field(..., example=1)
     title: str = Field(..., example="Букет")
     thumb_photo: str = Field(...)
+
+
+class Page(BasePage[T], Generic[T]):
+    @property
+    def info(self) -> str:
+        ru_title = {
+            'first': "Начало",
+            'last': "Конец",
+            'self': "Наверх",
+            'next': "Дальше",
+            'prev': "Назад",
+        }
+
+        def _get_wrap_link(title: str, link: str) -> str:
+            if link is None:
+                return ''
+            return f'<a href="{link}"><button>{ru_title[title]}</button><a/>'
+
+        gg = self.links.model_dump(include={'first', 'prev', 'next', 'last'})
+        opt = []
+        opt.append(_get_wrap_link('first', gg['first']))
+        opt.append(_get_wrap_link('prev', gg['prev']))
+        opt.append(_get_wrap_link('next', gg['next']))
+        opt.append(_get_wrap_link('last', gg['last']))
+        options = "\n".join(opt)
+        return f"<div>{options}</div>"
